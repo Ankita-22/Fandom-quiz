bb4c81dc16abf71e3100920d3dedde368f66bcd0
ankita panda <50979513+Ankita-22@users.noreply.github.com>
2021-06-28 04:17:17 +0000

    Added few details

--- index.js
+++ index.js
@@ -20,32 +20,26 @@ console.log("*************")
 
 var levelOne = [{
     question: "How many seasons of Friends are there ?",
-    options: ["10", "8", "5"],
     answer: "10",
   },
   {
     question: "What is name of the barcalounge in Joyes place?",
-    options: ["Rosita", "Lisa"],
     answer: "Rosita"
   },
   {
     question: "Joey played Dr.Drake Ramoray on which Soap Opera?",
-    options: ["DaysOfOurLives", "MacNCheese", "Alpachino"],
     answer: "DaysOfOurLives"
   },
   {
     question: "What is Chandler'sBing Job?",
-    options: ["Transponster", "NoBody knows"],
     answer: "NoBody knows"
   },
   {
     question: "What is Chandler'sBing middle name?",
-    options: ["Muriel", "Manny "],
     answer: "Muriel"
   },
   {
     question: "Which of the Joey's sisters did Chandler fool around?",
-    options: ["Mary Queen", "Mary Angela"],
     answer: "Mary Angela"
   },
   
@@ -69,32 +63,26 @@ if (score===6) {
 var levelTwo = [
 {
     question: "What holidays did chandler hate?",
-    options: ["Christmas", "Thanksgiving"],
     answer: "Thanksgiving"
   },
   {
     question: "How many pages was Rachel's letter to Ross?",
-    options: [10, 20, 18],
     answer: 18
   },
   {
     question: " Who introduced Phoebe and Mike?",
-    options: ["Ross","Monica","Joey"],
     answer: "Joey"
   },
    {
     question: " What food caused Ross to get sick on Space Mountain?",
-    options: ["Pizza","Tacos","Sandwich"],
     answer: "Tacos"
   },
    {
     question: " What did mail man call?",
-    options: ["Ms.ChanendlerBong","ChandlerBing"],
     answer: "Ms.ChanendlerBong"
   },
   {
     question: " What is Joeys favourite food?",
-    options: ["Pizza","Sandwiches"],
     answer: "Sandwiches"
   },
   ];
